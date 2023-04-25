966588a2a66877d1f14d9dd9d6385372468368a9
David Nevarez <david.nevarez03@hotmail.com>
2023-04-25 16:19:10 -0700

    update code for easy integration with the API logging middleware

--- api/v1/middlewares/errorHandler.ts
+++ api/v1/middlewares/errorHandler.ts
@@ -1,24 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 
-type ErrorResponse = {
-  status: number;
-  message: string;
-  error?: any;
+export const errorHandlingMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.locals.error = err;
+  next(err);
 };
-
-export const errorHandler =
-  (status: number, message: string, error?: any) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    const errorResponse: ErrorResponse = { status, message, error };
-
-    if (!errorResponse.error) {
-      delete errorResponse.error;
-    }
-
-    res.status(status).json(errorResponse);
-  };
-
-export const catchAsyncErrors =
-  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
-  };
