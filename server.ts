73cf365a159e4447ba372470fbd6ac5f4bdca2ac
David Nevarez <david.nevarez03@hotmail.com>
2023-04-25 16:23:46 -0700

    added `errorHandlingMiddleware` middleware

--- server.ts
+++ server.ts
@@ -10,12 +10,14 @@ import connectDB from "./config/database";
 
 // middlewares
 import { logRequests, logResponses } from "./api/v1/middlewares/logging";
+import { errorHandlingMiddleware } from "./api/v1/middlewares/errorHandler";
 
 const startServer = async () => {
   try {
     await connectDB(database.url as string);
     server
       .use(logRequests)
+      .use(errorHandlingMiddleware)
       .use(logResponses)
       .listen(port, () => {
         console.log(`Server running at http://localhost:${port}`);
