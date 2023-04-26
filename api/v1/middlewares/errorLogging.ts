ec7add4023b61fe753cc664fe6b9a86868fa2144
David Nevarez <david.nevarez03@hotmail.com>
2023-04-26 11:12:58 -0700

    updated error logging

--- api/v1/middlewares/errorLogging.ts
+++ api/v1/middlewares/errorLogging.ts
@@ -2,12 +2,37 @@
 import { Request, Response, NextFunction } from "express";
 import { logger } from "./logging";
 
+const formatTime = (time: string) => {
+  // Formats the string representing the time to the local time of the user
+  const date = new Date(time);
+  const userDate = new Date(
+    date.getTime() - date.getTimezoneOffset() * 60 * 1000
+  );
+
+  // You can adjust options as needed
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: "UTC",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
+  return userDate.toLocaleString(undefined, options);
+};
+
 // Function to log errors
-const logError = (error: Error) => {
+const logError = (error: Error, req: Request) => {
   logger.error({
     message: "Error",
     error: error.message,
-    timestamp: new Date().toISOString(),
+    // log the user's IP address
+    ip: req.ip,
+    // log the API address
+    apiAddress: req.originalUrl,
+    timestamp: formatTime(new Date().toISOString()),
   });
 };
 
@@ -21,6 +46,6 @@ export const errorLoggingMiddleware = (
     status: 500,
     message: err.message,
   });
-  logError(err);
+  logError(err, req);
   next(err);
 };
