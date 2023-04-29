3ea0350f04d946fd200770f031de26b826c01c4c
David Nevarez <david.nevarez03@hotmail.com>
2023-04-29 00:16:34 -0700

    simplified code

--- utils/errorHandlers.ts
+++ utils/errorHandlers.ts
@@ -1,62 +1,18 @@
-export const createUnauthError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 401,
-});
+type CustomError = { message: string; statusCode: number };
 
-export const createNotFoundError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 404,
-});
+const createError =
+  (statusCode: number) =>
+  (message: string): CustomError => ({
+    message,
+    statusCode,
+  });
 
-export const createBadRequestError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 400,
-});
-
-export const createForbiddenError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 403,
-});
-
-export const createInternalServerError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 500,
-});
-
-export const createConflictError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 409,
-});
-
-export const createValidationError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 422,
-});
-
-export const createUnauthorizedError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 401,
-});
-
-export const createNotImplementedError = (
-  message: string
-): { message: string; statusCode: number } => ({
-  message,
-  statusCode: 501,
-});
+export const createUnauthError = createError(401);
+export const createNotFoundError = createError(404);
+export const createBadRequestError = createError(400);
+export const createForbiddenError = createError(403);
+export const createInternalServerError = createError(500);
+export const createConflictError = createError(409);
+export const createValidationError = createError(422);
+export const createUnauthorizedError = createError(401);
+export const createNotImplementedError = createError(501);
